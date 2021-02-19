bf91d376dc55cc559c5ff190d8989c4c52f3ecb4
Andy.Chen <f107110126@gmail.com>
2021-02-19 11:41:43 +0800

    stage 2021.02.19 - file storage test

--- app-src/app/Http/Controllers/FirmwareController.php
+++ app-src/app/Http/Controllers/FirmwareController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Device;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FirmwareController extends Controller
 {
@@ -15,12 +16,26 @@ class FirmwareController extends Controller
 
     public function list(Device $device)
     {
-        $firmwares = $device->firmwares;
-        return view('firmwares.index', compact('device', 'firmwares'));
+        return view('firmwares.index', compact('device'));
     }
 
     public function create(Device $device)
     {
-        return $device;
+        return view('firmwares.create', compact('device'));
+    }
+
+    public function store(Device $device)
+    {
+        request()->validate([
+            'version' => 'required',
+            'checksum' => 'required|file',
+            'firmware' => 'required|file'
+        ]);
+        // $checksum = request()->checksum->store('test', 'public');
+        // $checksum = 'test/dcwVTroxa3DvLnWrodyMZqVvw4svAWegbEqihquE.txt';
+        // $checksum_url = Storage::disk('public')->url($checksum);
+        // $checksum_url = str_replace(env('APP_URL'), url('/'), $checksum_url); // directly access
+        // $download = Storage::disk('public')->download($checksum, 'checksum'); // directly download
+        // return $checksum_url;
     }
 }
