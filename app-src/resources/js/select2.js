04d7a46143d72ca0d5bb1563fc6d6b86a0d271d7
Andy.Chen <f107110126@gmail.com>
2021-03-02 10:16:00 +0800

    stage - 2021.03.02 - init select2 after css loaded

--- app-src/resources/js/select2.js
+++ app-src/resources/js/select2.js
@@ -1,11 +1,33 @@
 let init_select2 = (event) => {
     $('select.select2').map((index, item) => {
-        console.log(event, item, getComputedStyle(item).width);
         $(item).select2({
-            closeOnSelect: item.getAttribute('multiple') === "",
+            // closeOnSelect: !item.hasAttribute('multiple'),
             tags: item.classList.contains('tags')
         });
     });
 };
 
-$(document).on('DOMContentLoaded load ready turbolinks:load', init_select2);
+let interval, counter = 0, limit = 100;
+interval = window.setInterval(function () {
+    let styles = document.styleSheets;
+    for (let index = 0; index < styles.length; index++) {
+        let classes, result, list = ['rules', 'cssRules'];
+        list.map((item) => {
+            try {
+                classes = styles[index][item];
+            } catch (e) {
+                return;
+            }
+            for (let index2 = 0; index2 < classes.length; index2++) {
+                if (classes[index2].selectorText === '.form-control') result = classes[index2];
+                else continue;
+            }
+        });
+        if (result) {
+            window.clearInterval(interval);
+            init_select2();
+        }
+    }
+    counter++;
+    if (counter >= limit) window.clearInterval(interval);
+}, 1);
