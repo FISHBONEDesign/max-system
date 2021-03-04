a6ee2bd633a95e9c627488e94094f7a7935ec610
Andy.Chen <f107110126@gmail.com>
2021-03-04 10:47:11 +0800

    add policy to group

--- app-src/resources/views/groups/show-component.blade.php
+++ app-src/resources/views/groups/show-component.blade.php
@@ -1,18 +1,22 @@
 <a href="{{ route('admin.groups.index') }}" class="btn btn-outline-dark mb-2">{{ '< Groups' }}</a>
 <div class="card mb-2">
-    <div class="card-header">{{ $group->name }} &nbsp;&nbsp;&nbsp;
-        <a href="{{ route('admin.groups.edit', $group) }}" class="text-success">
-            <i class="fas fa-edit"></i>
-        </a>
-        <button type="button" class="text-danger"
-            onclick="event.preventDefault(); if (window.confirm('are you sure to delete the group and contents?')) document.getElementById('delete-group-{{ $group->id }}').submit();">
-            <i class="fas fa-trash-alt"></i>
-        </button>
-        <form id="delete-group-{{ $group->id }}" method="post"
-            action="{{ route('admin.groups.destroy', $group) }}">
-            @csrf
-            @method('delete')
-        </form>
+    <div class="card-header">{{ $group->name }}
+        @can('update', $group)
+            <a href="{{ route('admin.groups.edit', $group) }}" class="ml-3 text-success">
+                <i class="fas fa-edit"></i>
+            </a>
+        @endcan
+        @can('update', $group)
+            <button type="button" class="text-danger"
+                onclick="event.preventDefault(); if (window.confirm('are you sure to delete the group and contents?')) document.getElementById('delete-group-{{ $group->id }}').submit();">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+            <form id="delete-group-{{ $group->id }}" method="post"
+                action="{{ route('admin.groups.destroy', $group) }}">
+                @csrf
+                @method('delete')
+            </form>
+        @endcan
     </div>
 
     <div class="card-body">
