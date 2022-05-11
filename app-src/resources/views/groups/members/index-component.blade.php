9847c0104a1ca81d07381395dbdd09167d302481
Ivan_Ma <ivan@rexlite.com.tw>
2022-05-11 09:36:42 +0800

    判斷使用者編輯group member 的權限

--- app-src/resources/views/groups/members/index-component.blade.php
+++ app-src/resources/views/groups/members/index-component.blade.php
@@ -7,13 +7,34 @@
         <div class="card-body">
             <div class="mb-2">
                 @can('update', $group)
-                    <a href="{{ route('admin.groups.members.edit', [$member->group, $member]) }}">
-                        Name: {{ $member->name }} <br>
+                    @if ($group->projects->admin_id == $member->admin_id || auth('admin')->user()->id == $member->admin_id)
+                        {{ ($group->projects->admin_id === $member->admin_id) ? 'Project Owner: ': 'Name: '}}
+                        {{ $member->name }} <br>
                         Editable: {{ $member->edit ? 'true' : 'false' }} <br>
                         created at: {{ $member->created_at }} <br>
                         updated at: {{ $member->updated_at }}
-                    </a>
-                    @elsecan('view', $group)
+                    @else
+                        <a href="{{ route('admin.groups.members.edit', [$member->group, $member]) }}">
+                            Name: {{ $member->name }} <br>
+                            Editable: {{ $member->edit ? 'true' : 'false' }} <br>
+                            created at: {{ $member->created_at }} <br>
+                            updated at: {{ $member->updated_at }}
+                        </a>
+                        <br>
+                        @php
+                            $delete_form_id = "group-{$member->group->id}-member-{$member->id}-delete";
+                        @endphp
+                        <button type="button" class="btn btn-danger"
+                            onclick="if (window.confirm('are you sure remove {{ addslashes($member->name) }} from {{ addslashes($member->group->name) }}?')) document.querySelector('form#{{ $delete_form_id }}').submit();">
+                            <i class="fas fa-trash-alt"></i> Remove User
+                        </button>
+                        <form id="{{ $delete_form_id }}" method="post"
+                            action="{{ route('admin.groups.members.destroy', [$member->group, $member]) }}">
+                            @csrf
+                            @method('delete')
+                        </form>
+                    @endif
+                @elsecan('view', $group)
                     Name: {{ $member->name }} <br>
                     Editable: {{ $member->edit ? 'true' : 'false' }} <br>
                     created at: {{ $member->created_at }} <br>
@@ -21,18 +42,6 @@
                 @endcan
             </div>
             @can('update', $group)
-                @php
-                    $delete_form_id = "group-{$member->group->id}-member-{$member->id}-delete";
-                @endphp
-                <button type="button" class="btn btn-danger"
-                    onclick="if (window.confirm('are you sure remove {{ addslashes($member->name) }} from {{ addslashes($member->group->name) }}?')) document.querySelector('form#{{ $delete_form_id }}').submit();">
-                    <i class="fas fa-trash-alt"></i> Remove User
-                </button>
-                <form id="{{ $delete_form_id }}" method="post"
-                    action="{{ route('admin.groups.members.destroy', [$member->group, $member]) }}">
-                    @csrf
-                    @method('delete')
-                </form>
             @endcan
         </div>
     </div>
