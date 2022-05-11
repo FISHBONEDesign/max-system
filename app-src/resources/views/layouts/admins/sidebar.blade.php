263a1194119efe48562bcde0c2cb196b3838b868
Ivan_Ma <ivan@rexlite.com.tw>
2022-05-11 17:52:25 +0800

    sidebar調整

--- app-src/resources/views/layouts/admins/sidebar.blade.php
+++ app-src/resources/views/layouts/admins/sidebar.blade.php
@@ -34,13 +34,32 @@
             <p class="list-group-item list-group-item-action bg-light text-muted">No any shared project</p>
         @endforelse
 
+        {{-- Other Projects --}}
+        {{-- @php
+            $user = auth()->user();
+        @endphp
+        @if (auth('admin')->user()->isSupervisor)
+            <p class="list-group-item bg-secondary" style="color: white"><b>Other Project</b></p>
+            @forelse ($projects->diff($user->projects)->diff($user->shared_projects) as $project)
+                @php
+                    $name = $project->name;
+                    $path = route('admin.projects.show', $project);
+                    $isActive = request()->is(preg_replace('/^\/?(.*)$/', '$1/*', route('admin.projects.show', $project, false)));
+                @endphp
+                <a href="{{ $path }}"
+                    class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">{{ $name }}</a>
+            @empty
+                <p class="list-group-item list-group-item-action bg-light text-muted">No any project</p>
+            @endforelse
+        @endif --}}
+
         {{-- Groups --}}
         {{-- <p class="list-group-item">Groups</p> --}}
-        @php
+        {{-- @php
             $isActive = request()->routeIs('admin.groups.*');
         @endphp
         <a href="{{ route('admin.groups.index') }}"
-            class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">Groups</a>
+            class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">Groups</a> --}}
 
         {{-- <a href="{{ route('admin.projects.devices.index') }}" class="list-group-item list-group-item-action @if (request()->routeIs('admin.manage.devices.*') || request()->routeIs('admin.manage.firmwares.*')) active @else bg-light @endif">Devices & Firmwares</a> --}}
     </div>
