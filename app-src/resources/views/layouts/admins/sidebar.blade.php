5b35d7ffdb5e6956ee9f89091d866e9e95911c59
Ivan_Ma <ivan@rexlite.com.tw>
2022-05-11 09:37:11 +0800

    增加標示

--- app-src/resources/views/layouts/admins/sidebar.blade.php
+++ app-src/resources/views/layouts/admins/sidebar.blade.php
@@ -1,37 +1,46 @@
 <div class="sidebar-wrapper bg-light border-right">
-    <div class="sidebar-heading">Max System Admin</div>
+    <div class="sidebar-heading">{{ config('app.name', 'Laravel') }} Menu</div>
     <div class="list-group list-group-flush">
-        <a href="{{ route('admin.home') }}" class="list-group-item list-group-item-action @if (request()->is('admin/projects')) active @else bg-light @endif">Dashboard</a>
+        <a href="{{ route('admin.home') }}"
+            class="list-group-item list-group-item-action @if (request()->is('admin/projects')) active @else bg-light @endif">Dashboard</a>
 
         {{-- Projects --}}
-        @forelse (auth('admin')->user()->projects as $project)
-            @php
-                $name = $project->name;
-                $path = route('admin.projects.show', $project);
-                $isActive = request()->is(preg_replace('/^\/?(.*)$/', '$1/*', route('admin.projects.show', $project, false)));
-            @endphp
-            <a href="{{ $path }}" class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">{{ $name }}</a>
-        @empty
-
-        @endforelse
+        <div>
+            <p class="list-group-item bg-secondary" style="color: white"><b>My Projects</b></p>
+            @forelse (auth('admin')->user()->projects as $project)
+                @php
+                    $name = $project->name;
+                    $path = route('admin.projects.show', $project);
+                    $isActive = request()->is(preg_replace('/^\/?(.*)$/', '$1/*', route('admin.projects.show', $project, false)));
+                @endphp
+                <a href="{{ $path }}"
+                    class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">{{ $name }}</a>
+            @empty
+                <p class="list-group-item list-group-item-action bg-light text-muted">No any project</p>
+            @endforelse
+        </div>
 
         {{-- Shared Projects --}}
+        <p class="list-group-item bg-secondary" style="color: white"><b>Shared Project</b></p>
         @forelse (auth('admin')->user()->shared_projects as $project)
             @php
                 $name = $project->name;
                 $path = route('admin.projects.show', $project);
                 $isActive = request()->is(preg_replace('/^\/?(.*)$/', '$1/*', route('admin.projects.show', $project, false)));
             @endphp
-            <a href="{{ $path }}" class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">{{ $name }}</a>
+            <a href="{{ $path }}"
+                class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">{{ $name }}</a>
         @empty
-
+            <p class="list-group-item list-group-item-action bg-light text-muted">No any shared project</p>
         @endforelse
 
         {{-- Groups --}}
+        {{-- <p class="list-group-item">Groups</p> --}}
         @php
             $isActive = request()->routeIs('admin.groups.*');
         @endphp
-        <a href="{{ route('admin.groups.index') }}" class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">Groups</a>
+        <a href="{{ route('admin.groups.index') }}"
+            class="list-group-item list-group-item-action @if ($isActive) active @else bg-light @endif">Groups</a>
 
         {{-- <a href="{{ route('admin.projects.devices.index') }}" class="list-group-item list-group-item-action @if (request()->routeIs('admin.manage.devices.*') || request()->routeIs('admin.manage.firmwares.*')) active @else bg-light @endif">Devices & Firmwares</a> --}}
     </div>
